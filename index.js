2a1c7f151721b6867b75249b0ec4d36be8b1392c
Andrew Kuzmenko <kuzmenko@ucoz-team.net>
2019-11-09 03:28:19 +0300

    copypaste random bot 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //COPYPASTE BOT
+
 function rand() {
   return Math.round(Math.random());
 }
@@ -7,8 +8,18 @@ let { x, y } = API.getCurrentPosition();
 const maxDistance =  API.getActionPointsCount();
 if (rand()) {
   x = rand() ? x + maxDistance : x - maxDistance;
+  if (x <= 0) {
+    x += 1;
+  } else if (x >= API.getArenaSize() - 1) {
+    x -= 1;
+  }
 } else {
   y = rand() ? y + maxDistance : y - maxDistance;
+  if (y <= 0) {
+    y += 1;
+  } else if (y >= API.getArenaSize() - 1) {
+    y -= 1;
+  }
 }
 
 API.move(x, y);
