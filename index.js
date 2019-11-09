1b8df625cf1c9f3d8864f8147dbb533c26c0d17d
Andrew Kuzmenko <kuzmenko@ucoz-team.net>
2019-11-09 15:20:17 +0300

    ver2 lazybot

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//COPYPASTE BOT
 
 function rand() {
   return Math.round(Math.random());
@@ -6,20 +5,76 @@ function rand() {
 
 let { x, y } = API.getCurrentPosition();
 const maxDistance =  API.getActionPointsCount();
-if (rand()) {
-  x = rand() ? x + maxDistance : x - maxDistance;
-  if (x <= 0) {
-    x += 1;
-  } else if (x >= API.getArenaSize() - 1) {
-    x -= 1;
+const arenaSize = API.getArenaSize();
+
+const maxX = x => (x + maxDistance) % arenaSize;
+const minX = x => (x - maxDistance + arenaSize) % arenaSize;
+
+const maxY = y => (y + maxDistance) % arenaSize;
+const minY = y => (y - maxDistance + arenaSize) % arenaSize;
+
+const otherBots = API.getEnemies();
+const canKillBots = otherBots.filter(({position}) => {
+  if (position.x >= minX(x) && position.x <=maxX(x) && position.y >= minY(y) && position.y <=maxY(y)) {
+    return true;
+  }
+});
+if(canKillBots.length) {
+  let canSafeKillBots = canKillBots.filter(({position}) => {
+    const notSafe = otherBots.some(({position : otherPositon}) => {
+      if (otherPositon.x >= minX(position.x)
+          && otherPositon.x <=maxX(position.x)
+          && otherPositon.y >= minY(position.y)
+          && otherPositon.y <=maxY(position.y)) {
+        return true;
+      }
+    });
+    return !notSafe;
+  });
+  // пока так
+  canSafeKillBots = canKillBots;
+  console.log(JSON.stringify(canSafeKillBots));
+  if (canSafeKillBots.length){
+    const target = canSafeKillBots.pop();
+    x = target.position.x;
+    y = target.position.y;
+  } else {
+    // валим в рандом
+// валим в рандом
+    if (rand()) {
+      x = rand() ? x + maxDistance : x - maxDistance;
+      if (x <= 0) {
+        x += 1;
+      } else if (x >= API.getArenaSize() - 1) {
+        x -= 1;
+      }
+    } else {
+      y = rand() ? y + maxDistance : y - maxDistance;
+      if (y <= 0) {
+        y += 1;
+      } else if (y >= API.getArenaSize() - 1) {
+        y -= 1;
+      }
+    }
   }
 } else {
-  y = rand() ? y + maxDistance : y - maxDistance;
-  if (y <= 0) {
-    y += 1;
-  } else if (y >= API.getArenaSize() - 1) {
-    y -= 1;
+  // валим в рандом
+  if (rand()) {
+    x = rand() ? x + maxDistance : x - maxDistance;
+    if (x <= 0) {
+      x += 1;
+    } else if (x >= API.getArenaSize() - 1) {
+      x -= 1;
+    }
+  } else {
+    y = rand() ? y + maxDistance : y - maxDistance;
+    if (y <= 0) {
+      y += 1;
+    } else if (y >= API.getArenaSize() - 1) {
+      y -= 1;
+    }
   }
 }
+console.log(x,y);
 
 API.move(x, y);
